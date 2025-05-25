5ad17eb267cf592ef4436cb54b6faf903699777e
LCsiu <148622803+LouisCaiKM7@users.noreply.github.com>
2025-05-25 21:51:44 +0800

    hm0a

--- hw0a/src/JavaExercises.java
+++ hw0a/src/JavaExercises.java
@@ -1,3 +1,5 @@
+import javax.tools.JavaCompiler;
+
 /**
  * Skeleton file for HW0A.
  * Exercises sourced from Practice-It by the University of Washington.
@@ -12,15 +14,45 @@ public class JavaExercises {
      * The first row contains 1 star, the second 2 stars, and so on.
      */
     public static void starTriangle() {
-        // TODO: Fill in this function
+        int wanted_line = 5;
+        for(int j=0; j<wanted_line; j++){
+            printStar(j+1);
+            System.out.print("\n");
+        }
     }
+    public static void printStar(int number){
+        for(int i=0;i<number;i++){
+            System.out.print("*");
+        }
+    }
+
 
     /**
      * Prints each character of a given string followed by its reverse index.
      * Example: printIndexed("hello") -> h4e3l2l1o0
      */
     public static void printIndexed(String s) {
-        // TODO: Fill in this function
+        for(int i = 0; i<s.length(); i++){
+            System.out.print(stringToPrint(s)[i]);
+            System.out.print(getLength(s)[i]);
+        }
+        System.out.println();
+    }
+    public static char[] stringToPrint(String string){
+        char[] charArray = new char[string.length()];
+        for(int i = 0; i<string.length(); i++){
+            charArray[i] = string.charAt(i);
+        }
+        return charArray;
+    }
+    public static int[] getLength(String string){
+        int[] numList = new int[string.length()];
+        int j = 0;
+        for (int i= string.length(); i >0; i--){
+            numList[j]=i;
+            j++;
+        }
+        return numList;
     }
 
     /**
@@ -28,8 +60,12 @@ public class JavaExercises {
      * Example: stutter("hello") -> "hheelllloo"
      */
     public static String stutter(String s) {
-        // TODO: Fill in this function
-        return null;
+        String toPrint= "";
+        for(int i = 0; i<s.length(); i++){
+            toPrint+=stringToPrint(s)[i];
+            toPrint+=stringToPrint(s)[i];
+        }
+        return toPrint;
     }
 
     /**
@@ -42,8 +78,15 @@ public class JavaExercises {
      *   0 if the point lies on an axis.
      */
     public static int quadrant(int x, int y) {
-        // TODO: Fill in this function
-        return 0;
+        if(x>0 && y>0){
+            return 1;
+        }else if (x<0 && y>0){
+            return 2;
+        } else if (x<0 && y<0) {
+            return 3;
+        }else{
+            return 4;
+        }
     }
 
     public static void main(String[] args) {
